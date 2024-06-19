55c8b029b95903fee1c19c869e8607c80f27e16a
n-skot <ricardo.nskot@gmail.com>
2024-06-18 20:34:30 -0600

    init socket

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import "dotenv/config"
+
+import express from "express";
+import config from "./config/index.js";
+import path from "path";
+
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { fileURLToPath } from "url";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer /*, { cors: { origin: "*" } }*/);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "/views")));
+
+app.get("/", (req, res) => {
+    console.log('dirname', __dirname);
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+// Socket.io event listeners
+io.on("connection", socket => {
+  console.log("Clients conectados", io.engine.clientsCount);
+  console.log("New client connected", socket.id);
+
+  // Handle disconnect event
+  socket.on("disconnect", () => {
+    console.log("Client disconnected", socket.id);
+  });
+
+  /*/ Handle message event
+  socket.on("message", (message) => {
+    console.log(`Received message: ${message}`);
+    io.emit("message", message);
+  });*/
+});
+
+httpServer.listen(config.port, () => { console.log("Server listo en ", config.port); });
